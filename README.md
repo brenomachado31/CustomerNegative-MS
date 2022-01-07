051cb6a5261b779b2bd11cfd2fcb7a7ba00ba297
Breno Machado <75369213+brenomachado31@users.noreply.github.com>
2022-01-07 01:24:47 -0300

    Removed

--- README.md
+++
@@ -1,1 +0,0 @@
-# CustomerNegative-MS
--- tests/customerNegativeTests.js
+++
@@ -1,8 +0,0 @@
-const customerNegative = require("../src/Service/customerNegative");
-
-function ExecTest() {
-    const customerNegativeExec = new customerNegative({Id: "123", InternalId: "123", Blocked: true});
-    customerNegativeExec.CheckStatusBlocked();
-}
-
-ExecTest();
